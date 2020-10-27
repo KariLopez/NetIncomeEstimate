1e65428467514cd53dfa0fbdad36e3239f364c16
KARINA LOPEZ <klopez@nhaschools.com>
2020-10-27 17:57:08 -0400

    add test suite and ability to test from vs

---
+++ NetIncomeEstimate/Tests/ValidationTests.cs
@@ -0,0 +1,43 @@
+﻿using NetIncomeEstimate.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetIncomeEstimate.Tests
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void TestValidateRepeatInput()
+        {
+            string input = "no";
+            bool expected = false;
+            var validation = new UserValidation();
+            bool actual = validation.ValidateRepeatInput(input);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestValidateGrossIncome()
+        {
+            string incomeInput = "60k";
+            var expected = false;
+
+            var validation = new UserValidation();
+            bool actual = validation.ValidateGrossIncome(incomeInput);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestValidateFilingStatus()
+        {
+            string input = "Single";
+            var expected = false;
+
+            var validation = new UserValidation();
+            bool actual = validation.ValidateFilingStatus(input);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
